62d64766f2163914108240bf699a2de6b4d5c5b7
yirano <yirano@protonmail.ch>
2019-12-08 13:00:29 -0500

    Set up 2d array challenge

--- HackerRank/Javascript/chocolate-feast.js
+++ HackerRank/Javascript/chocolate-feast.js
@@ -1,25 +1,25 @@
 function chocolateFeast(n, c, m) {
-  let ct = 0;
-  let wr = 0;
+	let ct = 0;
+	let wr = 0;
 
-  wr = ct = n / c;
-  console.log('wr beginning: ', wr);
-  console.log('ct beginning: ', ct);
-  console.log('cost of wrapper for choc: ', m);
-  console.log("---------------");
+	wr = ct = n / c;
+	console.log('wr beginning: ', wr);
+	console.log('ct beginning: ', ct);
+	console.log('cost of wrapper for choc: ', m);
+	console.log('---------------');
 
-  while (wr >= m) {
-    let tempCt = Math.floor(wr / m);
-    console.log('tempCt: ', tempCt);
-    ct += tempCt;
-    console.log('ct: ', ct);
-    wr %= m;
-    console.log('additional chocolate wrapper: ', wr);
-    wr += tempCt;
-    console.log('wrapper in total: ', wr);
-    console.log("---------------");
-  }
-  return Math.floor(ct);
+	while (wr >= m) {
+		let tempCt = Math.floor(wr / m);
+		console.log('tempCt: ', tempCt);
+		ct += tempCt;
+		console.log('ct: ', ct);
+		wr %= m;
+		console.log('additional chocolate wrapper: ', wr);
+		wr += tempCt;
+		console.log('wrapper in total: ', wr);
+		console.log('---------------');
+	}
+	return Math.floor(ct);
 }
 
 let n = 15;
