e3ba7d94e21e561f6ecf3022d71dd51b48b0237f
yirano <yirano@protonmail.ch>
2019-12-20 11:37:31 -0500

    Add work in progress solution to New Year Choas

--- HackerRank/Javascript/new-year-chaos.js
+++ HackerRank/Javascript/new-year-chaos.js
@@ -3,11 +3,28 @@ function minimumBribes(q) {
 	let people = q[1];
 	let finalQue = q[2];
 	let que = [];
+	let ct = 0;
+	let bound;
+	let ans;
 
-	for (let i = 1; i <= people; i++) {
-		que.push(i);
+	// for (let i = 1; i <= people; i++) {
+	// 	que.push(i);
+	// }
+
+	for (let j = 0; j < finalQue.length; j++) {
+		bound = finalQue[j] - finalQue[(j += 1)];
+		if (bound >= 1) {
+			ct += bound;
+		}
 	}
-	console.log(que);
+	if (ct > limit) {
+		ans = [ ct, 'Too Chaotic' ];
+	} else {
+		ans = ct;
+	}
+	console.log(ans);
+	return ans;
 }
 
 minimumBribes([ 2, 5, [ 2, 1, 5, 3, 4 ] ]);
+minimumBribes([ 1, 8, [ 1, 2, 5, 3, 4, 7, 8, 6 ] ]);
